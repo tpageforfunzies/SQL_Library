6a663b9fc67f01a9bb816729461dfef873e8415f
tpageforfunzies <tjpage@indiana.edu>
2017-11-18 18:54:29 -0500

    added ALTER

---
+++ 0.07_ALTER.sql
@@ -0,0 +1,22 @@
+﻿--Add a column to the table called Manufacturer
+ALTER TABLE Products
+	ADD Manufacturer text
+GO
+
+
+--Drop Manufacturer
+ALTER TABLE Products
+	DROP COLUMN Manufacturer
+GO
+
+--Add UPC Code
+ALTER TABLE Products
+	ADD UPCCode int
+GO
+
+--Drop UPC Code
+ALTER TABLE Products
+	DROP COLUMN UPCCode
+GO
+
+
