a498b9811c455a36d6070fd9cdc51ad5c9205577
tpageforfunzies <tjpage@indiana.edu>
2017-11-18 18:25:31 -0500

    added UPDATE

---
+++ 0.05_UPDATE.sql
@@ -0,0 +1,18 @@
+﻿--Changing the productname of second product from 
+--screwdriver to Flat Head Screwdriver
+UPDATE Products
+	SET ProductName = 'Flat Head Screwdriver'
+	WHERE ProductID = 50
+GO
+
+--Changing the price of first product from 12.48 to 12.50
+UPDATE Products
+	SET Price = 12.50
+	WHERE ProductID = 1
+GO
+
+--Changing the price of all products with a certain price
+UPDATE Products
+	SET Price = 4.00
+	WHERE Price = 3.17
+GO
