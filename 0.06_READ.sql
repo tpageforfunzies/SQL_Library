94a682f8ab4540a0ea5cd4ef4e644711d0568b56
tpageforfunzies <tjpage@indiana.edu>
2017-11-18 18:33:29 -0500

    added READ

---
+++ 0.06_READ.sql
@@ -0,0 +1,32 @@
+﻿--Print all products in the table
+SELECT ProductID, ProductName, Price, ProductDescription
+	FROM Products
+GO
+
+--Selects all columns in the table using *
+SELECT * FROM Products
+GO
+
+--Selects and returns only two columns from table
+SELECT ProductName, Price
+	FROM Products
+GO
+
+--Select products based on criteria (productid < 60)
+SELECT * 
+	FROM Products
+	WHERE (ProductID < 60)
+GO
+
+--Select products based on criteria (price is 12.50)
+SELECT *
+	FROM Products
+	WHERE (Price = 12.50)
+GO
+
+--Select all products from table and returns productnames
+--and prices including a 7% tax, shows a temporary 
+--'customerpays' column
+SELECT ProductName, Price * 1.07 AS CustomerPays
+	FROM Products
+GO
