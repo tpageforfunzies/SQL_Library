2dd1958434a1d147bc62d151bb021f08e0a28035
tpageforfunzies <tjpage@indiana.edu>
2017-11-18 18:17:53 -0500

    insert queries, multiple syntax shown

---
+++ 0.04_INSERT.sql
@@ -0,0 +1,19 @@
+﻿--Standard Syntax
+INSERT Products (ProductID, ProductName, Price, ProductDescription)
+	VALUES (1, 'Clamp', 12.48, 'Workbench clamp')
+GO
+
+--Changing the order of the columns
+INSERT Products (ProductName, ProductID, Price, ProductDescription)
+	VALUES ('Screwdriver', 50, 3.17, 'Flathead')
+GO
+
+--Skipping the column list, but keeping values in order
+INSERT Products
+	VALUES (75, 'Tire Bar', NULL, 'Tool for changing tires.')
+GO
+
+--Dropping ProductDescription due to being nullable
+INSERT Products (ProductID, ProductName, Price)
+	Values (3000, '3mm Bracket', .52)
+GO
