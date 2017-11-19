1f4cda3d975b6d75bf70301bf9e18115d595ad2e
tpageforfunzies <tjpage@indiana.edu>
2017-11-18 19:58:06 -0500

    added PURCHASES_TABLES

---
+++ 0.09_PURCHASES_TABLE.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE dbo.Purchases (
+    PurchaseID INT IDENTITY (1, 1) NOT NULL,
+    ProductID  INT NOT NULL,
+    CustomerID INT NOT NULL,
+    Quantity   INT NULL,
+    CONSTRAINT [PK_dbo.Purchases] PRIMARY KEY CLUSTERED ([PurchaseID] ASC),
+    CONSTRAINT [FK_dbo.Purchases_dbo.Customer_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Purchases_dbo.Products_ProductID] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Products] ([ProductID]) ON DELETE CASCADE
+);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ProductID]
+    ON [dbo].[Purchases]([ProductID] ASC);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_CustomerID]
+    ON [dbo].[Purchases]([CustomerID] ASC);
