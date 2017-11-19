f2b0d1b9018fbe856f1df4417d4c9421cbc7a62a
tpageforfunzies <tjpage@indiana.edu>
2017-11-18 19:14:30 -0500

    added customer table with seed data

---
+++ 0.08_CUSTOMER.sql
@@ -0,0 +1,18 @@
+﻿--Create a customers table
+CREATE TABLE Customers (
+	CustomerID int PRIMARY KEY NOT NULL,
+	LastName nvarchar(25) NOT NULL,
+	FirstName nvarchar(25) NOT NULL,
+	LastPurchase Datetime NOT NULL)
+GO
+
+--Insert seed customers
+INSERT Customers
+	VALUES (1, 'Tinney', 'Zach', SYSDATETIME())
+INSERT Customers
+	VALUES (2, 'Wainscott', 'Chris', SYSDATETIME())
+INSERT Customers
+	VALUES (3, 'Aikins', 'Jenn', SYSDATETIME())
+INSERT Customers
+	VALUES (4, 'OConnor', 'Paul', SYSDATETIME())
+GO
