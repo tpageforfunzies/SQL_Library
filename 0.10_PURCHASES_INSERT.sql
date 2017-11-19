d44c907e9aabad7b0fa8440fa51ab6b8d84571a5
tpageforfunzies <tjpage@indiana.edu>
2017-11-18 20:04:25 -0500

    added PURCHASES_INSERT, added data to purchases table with PK and FKs

---
+++ 0.10_PURCHASES_INSERT.sql
@@ -0,0 +1,12 @@
+﻿SET IDENTITY_INSERT Purchases ON
+
+INSERT Purchases (PurchaseID, ProductID, CustomerID, Quantity)
+	VALUES (1, 1, 1, 5)
+
+INSERT Purchases (PurchaseID, ProductID, CustomerID, Quantity)
+	VALUES (2, 1, 2, 5)
+
+INSERT Purchases (PurchaseID, ProductID, CustomerID, Quantity)
+	VALUES (3, 50, 4, 3)
+
+GO
